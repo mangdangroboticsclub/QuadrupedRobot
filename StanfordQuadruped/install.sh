df57132f8eccff01fc04ee5b2b61a878c7fae906
Gavin Zhang <gavin.zhang@mangdang.net>
2021-12-05 20:20:59 +0800

    add shortcut for calibration tool

--- StanfordQuadruped/install.sh
+++ StanfordQuadruped/install.sh
@@ -8,3 +8,5 @@ sudo ln -s $(realpath .)/robot.service /etc/systemd/system/
 sudo systemctl daemon-reload
 sudo systemctl enable robot
 sudo systemctl start robot
+
+sudo mv CalibrationTool.desktop /home/ubuntu/Desktop
